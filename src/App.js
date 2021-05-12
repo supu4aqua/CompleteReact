3a6163c7af966f7c958a8af6a15671c6a3005c94
Supreet <supreet.kaur@live.com>
2021-05-12 16:03:13 -0400

    Removed data

--- src/App.js
+++ src/App.js
@@ -17,12 +17,6 @@ const App = () => {
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
   ];
   
   return (
